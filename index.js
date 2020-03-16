5d9e813cae0263b92c53dd4260907ed76b6e5f04
elf Pavlik <elf-pavlik@hackers4peace.net>
2020-03-16 14:30:34 -0600

    ready for staging

---
+++ index.js
@@ -0,0 +1,4 @@
+export { default as solutions } from './solutions.js'
+export { default as universities } from './universities.js'
+export { default as emailProviders } from './emailProviders.js'
+ 
---
+++ solutions.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    slug: "carbon-neutral-campus",
+    name: "Carbon neutral campus by 2030 or sooner",
+    description: "Carbon neutrality is achieved either through transitioning to renewable energy or buying offsets or both."
+  },
+  {
+    slug: "education-research",
+    name: "Increasing sustainability-related education and research",
+    description: "University courses, workshops, conferences, symposia, incentives for papers, etc..."
+  },
+  {
+    slug: "divestment",
+    name: "Divestment",
+    description: "From the university's endowment, selling financial assets and investements related to fossil fuel companies and ideally reinvest it in sustainable alternatives with competitive returns."
+  },
+  {
+    slug: "community-engagement",
+    name: "Community engagement and policy advocacy",
+    description: "Outreach, collaboration with NGOs and socially-minded enterprises, communication with government bodies, collaboration with the community, etc..."
+  },
+  {
+    slug: "collaboration",
+    name: "Collaboration with other universities",
+    description: "Theres plenty of universities with vibrant sustainability and climate innitiatives, let's find concrete ways to collaborate and have more impact."
+  },
+  {
+    slug: "green-campus",
+    name: "Green campus",
+    description: "Energy efficiency, zero waste programs, subsidized plant-based foods, shift from single-use to reusable packaging, composting, green areas, bike programs, etc..."
+  },
+]
