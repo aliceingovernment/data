72c7d6828ed9e585169a96adcc3d87cda02ec588
Sebastian Romero <tianfut@gmail.com>
2020-03-17 16:32:15 -0600

    Update emailProviders.js
    added around 15 common email domains

--- emailProviders.js
+++ emailProviders.js
@@ -1,6 +1,19 @@
 export default [
   "gmail.com",
   "hotmail.com",
-  "yahoo.com"
+  "yahoo.com",  
+  "outlook.com",
+  "icloud.com",
+  "aol.com",
+  "hotmail.co.uk",  
+  "hotmail.fr",
+  "msn.com",  
+  "orange.fr",
+  "yahoo.fr",
+  "mail.com",
+  "zoho.com",  
+  "protonmail.com",
+  "thunderbird.net",
+  "yandex.ru",
 ]
 
