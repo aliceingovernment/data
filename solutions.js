b6d148090ea4ed650c0905f832ab0aa29a6c2738
Sebastian Romero <tianfut@gmail.com>
2020-05-17 13:55:39 -0500

    Update solutions.js
    updated text

--- solutions.js
+++ solutions.js
@@ -1,32 +1,32 @@
 export default [
   {
     slug: "carbon-neutral-campus",
-    name: "Carbon neutral campus by 2030 or sooner",
+    name: "Carbon neutral campus by 2030 or earlier",
     description: "Carbon neutrality can be achieved through transitioning to renewable energy, buying offsets, or both."
   },
   {
     slug: "education-research",
     name: "Sustainability-related education and research",
-    description: "University courses, workshops, conferences, symposia, incentives for papers, etc..."
+    description: "Including university courses, workshops, conferences, symposia, incentives for papers on sustainability, etc."
   },
   {
     slug: "divestment",
     name: "Divestment",
-    description: "From the university's endowment, selling financial assets and investements related to fossil fuel companies and ideally reinvest it in sustainable alternatives with competitive returns."
+    description: "Selling the university's financial assets and investments related to fossil fuel companies and ideally reinvesting in sustainable alternatives with competitive returns."
   },
   {
     slug: "community-engagement",
     name: "Community engagement and policy advocacy",
-    description: "Outreach, collaboration with NGOs and socially-minded enterprises, communication with government bodies, collaboration with the community, etc..."
+    description: "Including outreach, collaboration with NGOs and socially-minded enterprises, communication with government bodies, collaboration with the community, etc."
   },
   {
     slug: "collaboration",
     name: "Collaboration between universities",
-    description: "Theres plenty of universities with vibrant sustainability and climate innitiatives, let's find concrete ways to collaborate and have more impact."
+    description: "There are lots of universities with vibrant sustainability and climate initiatives; let's find concrete ways to collaborate and have more impact."
   },
   {
     slug: "green-campus",
     name: "Green campus",
-    description: "Energy efficiency, zero waste programs, subsidized plant-based foods, shift from single-use to reusable packaging, composting, green areas, bike programs, etc..."
+    description: "Energy efficiency, zero-waste programs, subsidized plant-based foods, shifting from single-use to reusable packaging, composting, green areas, bike programs, etc."
   },
 ]
