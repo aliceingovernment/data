431b2e3a9ce31a82f5a37ab88008bc7113bac390
elf Pavlik <elf-pavlik@hackers4peace.net>
2020-08-18 15:22:10 -0500

    leave only links to mix with hipo list

--- universities.js
+++ universities.js
@@ -1,10 +1,6 @@
 export default [
   {
-    slug: "MIT",
-    name: "MIT",
-    domains: [
-      "mit.edu"
-    ],
+    domain: "mit.edu",
     links: [
       "https://www.mitclimateactionteam.org",
       "https://www.mit-divest.com",
@@ -12,324 +8,52 @@ export default [
     ]
   },
   {
-    slug: "unam",
-    name: "UNAM",
-    domains: [
-      "unam.mx"
-    ],
+    domain: "unam.mx",
     links: [
       "https://sustentabilidad.unam.mx/sustentabilidad-unam.php",
       "http://www.pincc.unam.mx/UC3_UNAM/Documentos/PAC-EUCC_UNAM_2018_FASE_1.pdf"
     ]
   },
   {
-    slug: "harvard",
-    name: "Harvard",
-    domains: [
-      "harvard.edu",
-      "hbs.edu"
-    ],
+    domain: "harvard.edu",
     links: [
       "https://www.harvardforward.org",
       "http://www.divestharvard.com"
     ]
   },
   {
-    slug: "stanford",
-    name: "Stanford",
-    domains: [
-      "stanford.edu"
-    ],
+    domain: "stanford.edu",
     links: [
       "https://www.fossilfreestanford.org",
       "https://twitter.com/DivestStanford"
     ]
   },
-   {
-    slug: "trinitycollegedublin",
-    name: "Trinity College Dublin",
-    domains: [
-      "tcd.ie"
-    ]
-  },
   {
-    slug: "itam",
-    name: "ITAM",
-    domains: [
-      "itam.mx"
-    ]
-  },
-  {
-    slug: "rutgers",
-    name: "Rutgers",
-    domains: [
-      "rutgers.edu"
-    ]
-  },
-  {
-    slug: "columbia",
-    name: "Columbia",
-    domains: [
-      "columbia.edu"
-    ]
-  },
-  {
-    slug: "princeton",
-    name: "Princeton",
-    domains: [
-      "princeton.edu"
-    ],
+    domain: "princeton.edu",
     links: [
       "divestprinceton.com"
     ]
   },
   {
-    slug: "cca",
-    name: "California College of the Arts",
-    domains: [
-      "cca.edu"
-    ]
-  },
-  {
-    slug: "mghihp",
-    name: "MGH Institute of Health Professions",
-    domains: [
-      "mghihp.edu"
-    ]
-  },
-  {
-    slug: "vassar",
-    name: "Vassar College",
-    domains: [
-      "vassar.edu"
-    ]
-  },
-  {
-    slug: "imperialcollegelondon",
-    name: "Imperial College London",
-    domains: [
-      "imperial.ac.uk"
-    ]
-  },
-  {
-    slug: "universityofminnesota",
-    name: "University of Minnesota",
-    domains: [
-      "umn.edu"
-    ]
-  },
-  {
-    slug: "newyorkuniversity",
-    name: "New York University",
-    domains: [
-      "nyu.edu"
-    ]
-  },
-  {
-    slug: "universityofillinoisaturbana-champaign",
-    name: "University of Illinois at Urbana-Champaign",
-    domains: [
-      "illinois.edu"
-    ]
-  },
-  {
-    slug: "berkeley",
-    name: "Berkeley",
-    domains: [
-      "berkeley.edu"
-    ],
+    domain: "berkeley.edu",
     links: [
       "https://sustainability.berkeley.edu/"
     ]
   },
   {
-    slug: "mbarara-university-of-science-and-technology",
-    name: "Mbarara University of Science and Technology",
-    domains: [
-      "must.ac.ug"
-    ]
-  },
-  {
-    slug: "university-of-nairobi",
-    name: "University of Nairobi",
-    domains: [
-      "uonbi.ac.ke"
-    ]
-  },
-  {
-    slug: "kenyatta-university",
-    name: "Kenyatta University",
-    domains: [
-      "ku.ac.ke"
-    ]
-  },
-  {
-    slug: "université-cheikh-anta-diop-de-dakar-sénégal",
-    name: "Université Cheikh Anta Diop de Dakar - Sénégal",
-    domains: [
-      "ucad.edu.sn"
-    ]
-  },
-  {
-    slug: "machakos-university",
-    name: "Machakos University",
-    domains: [
-      "mksu.ac.ke"
-    ]
-  },
-  {
-    slug: "makerere-university",
-    name: "Makerere University",
-    domains: [
-      "mak.ac.ug"
-    ]
-  },
-  {
-    slug: "kyambogo-university",
-    name: "Kyambogo University",
-    domains: [
-      "kyu.ac.ug"
-    ]
-  },
-  {
-    slug: "kaust",
-    name: "KAUST",
-    domains: [
-      "kaust.edu.sa"
-    ]
-  },
-  {
-    slug: "tec-de-monterrey",
-    name: "Tec de Monterrey",
-    domains: [
-      "itesm.mx"
-    ]
-  },
-  {
-    slug: "mcgill-university",
-    name: "McGill University",
-    domains: [
-      "mcgill.ca"
-    ]
-  },
-  {
-    slug: "concordia-university",
-    name: "Concordia University",
-    domains: [
-      "concordia.ca"
-    ]
-  },
-  {
-    slug: "universitat-de-barcelona",
-    name: "Universitat de Barcelona",
-    domains: [
-      "ub.edu"
-    ]
-  },
-  {
-    slug: "universidad-de-sao-paulo",
-    name: "Universidad de Sao Paulo",
-    domains: [
-      "usp.br"
-    ]
-  },
-  {
-    slug: "ku-leuven",
-    name: "KU Leuven",
-    domains: [
-      "kuleuven.be"
-    ]
-  },
-  {
-    slug: "wageningen-university-&-research",
-    name: "Wageningen University & Research",
-    domains: [
-      "wur.nl"
-    ],
+    domain: "wur.nl",
     links: [
       "https://www.wur.nl/en/Research-Results/Sustainable-Development-Goals.htm"
     ]
   },
   {
-    slug: "hec-paris",
-    name: "HEC Paris",
-    domains: [
-      "hec.edu"
-    ]
-  },
-  {
-    slug: "universidad-earth",
-    name: "Universidad EARTH",
-    domains: [
-      "earth.ac.ur"
-    ]
-  },
-  {
-    slug: "cornell",
-    name: "Cornell",
-    domains: [
-      "cornell.edu"
-    ]
-  },
-  {
-    slug: "georgetown",
-    name: "Georgetown",
-    domains: [
-      "georgetown.edu"
-    ]
-  },
-  {
-    slug: "ntu-singapore",
-    name: "NTU Singapore",
-    domains: [
-      "ntu.edu.sg"
-    ]
-  },
-    {
-    slug: "boston-college",
-    name: "Boston College",
-    domains: [
-      "bc.edu"
-    ],
+    domain: "bc.edu",
     links: [
       "https://www.instagram.com/bcclimatejustice/"
     ]
   },
-    {
-    slug: "monash-university",
-    name: "Monash University",
-    domains: [
-      "monash.edu"
-    ]
-  },
   {
-    slug: "hs-offenburg",
-    name: "Hochshule Offenburg",
-    domains: [
-      "hs-offenburg.de"
-    ]
-  },
-  {
-    slug: "the-univeristy-of-sydney",
-    name: "The University of Sydney",
-    domains: [
-      "sydney.edu.au"
-    ]
-  },
-  {
-    slug: "marquette-university",
-    name: "Marquette University",
-    domains: [
-      "marquette.edu"
-    ]
-  },
-  {
-    slug: "johns-hopkins-university",
-    name: "Johns Hopkins University",
-    domains: [
-      "jhu.edu",
-      "jh.edu"
-    ],
+    domain: "jhu.edu",
     links: [
       "https://www.instagram.com/refuelourfuture/",
       "https://www.facebook.com/RefuelOurFuture/",
@@ -337,101 +61,8 @@ export default [
       "https://sustainability.jhu.edu/"
     ]
   },
-   {
-    slug: "kampala-international-university",
-    name: "Kampala International University",
-    domains: [
-      "kiu.ac.ug"
-    ]
-  },
-   {
-    slug: "el-colegio-de-mexico",
-    name: "El Colegio de Mexico",
-    domains: [
-      "colmex.mx"
-    ]
-  },
-   {
-    slug: "american-university",
-    name: "American University",
-    domains: [
-      "american.edu"
-    ]
-  },
-   {
-    slug: "universidad-de-santiago-de-chile",
-    name: "Universidad de Santiago de Chile",
-    domains: [
-      "usach.cl"
-    ]
-  },
   {
-    slug: "carnegie-mellon-university",
-    name: "Carnegie Mellon University",
-    domains: [
-      "cmu.edu"
-    ]
-  },
-  {
-    slug: "dartmouth",
-    name: "Dartmouth",
-    domains: [
-      "dartmouth.edu"
-    ]
-  },
-  {
-    slug: "university-of-washington",
-    name: "University of Washington",
-    domains: [
-      "uw.edu",
-      "washington.edu"
-    ]
-  },
-  {
-    slug: "tufts-university",
-    name: "Tufts University",
-    domains: [
-      "tufts.edu"
-    ]
-  },
-  {
-    slug: "duke",
-    name: "Duke",
-    domains: [
-      "duke.edu"
-    ]
-  },
-  {
-    slug: "uc-san-diego",
-    name: "UC San Diego",
-    domains: [
-      "ucsd.edu"
-    ]
-  },
-  {
-    slug: "university-of-cambridge",
-    name: "University of Cambridge",
-    domains: [
-      "cam.ac.uk"
-    ]
-  },
-  {
-    slug: "boston-university",
-    name: "Boston University",
-    domains: [
-      "bu.edu"
-    ],
-    links: [
-      "https://www.instagram.com/divest_bu/",
-      "https://divestbu.com/"
-    ]
-  },
-  {
-    slug: "yale",
-    name: "Yale",
-    domains: [
-      "yale.edu"
-    ],
+    domain: "yale.edu",
     links: [
       "https://www.yaleforward.org",
       "https://openlab.yale.edu",
@@ -440,4 +71,3 @@ export default [
     ]
   }
 ]
-
