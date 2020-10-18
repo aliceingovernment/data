801b25084e43c2e440913a8aa9a2017e6c9d66ce
elf Pavlik <elf-pavlik@hackers4peace.net>
2020-10-18 11:54:02 -0500

    fix quote character

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { default as solutions } from './solutions.js'
 export { default as universities } from './universities.js'
 export { default as emailProviders } from './emailProviders.js'
-export { default as i18n } from './i18n.js` 
+export { default as i18n } from './i18n.js'
